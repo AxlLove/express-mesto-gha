0bc021fabc3393c6b4f5a980d3899b193371c3fe
Aleksey Kitaev <axlstarxx@gmail.com>
2022-05-30 10:08:56 +0300

    Pr14 iteration1/test10

--- routes/cards.js
+++ routes/cards.js
@@ -11,7 +11,7 @@ router.get('/cards', getCards);
 router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().pattern(urlRegExp),
+    link: Joi.string().required().pattern(urlRegExp),
   }),
 }), createCard);
 
